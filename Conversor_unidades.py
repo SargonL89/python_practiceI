117ee3c99b8e45141252311b324955f49123dd7d
Leandro Magnotti <leandro.magnotti@gmail.com>
2024-06-10 11:13:49 -0300

    Dollars to Pesos

--- Conversor_unidades.py
+++ Conversor_unidades.py
@@ -6,3 +6,12 @@ kilometers_to_miles = (kilometers * 1) / 1.61
 
 print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
 print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+dollars = 3000
+pesos_ars = 900000
+
+pesos_to_dollars = (pesos_ars * 1) / 1200
+dollars_to_pesos = (dollars * 1200) / 1
+
+print(pesos_ars, "pesos son", round(pesos_to_dollars, 1), "dólares")
+print(dollars, "dólares son", round(dollars_to_pesos, 1), "pesos")
