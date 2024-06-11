ca6b74ab3d8febff6752ee8c8f011f53c59da23c
Leandro Magnotti <leandro.magnotti@gmail.com>
2024-06-11 20:33:15 -0300

    Cálculo de duración de evento-reload

--- python_practice1.py
+++ python_practice1.py
@@ -1,39 +1,39 @@
 # Conversor de unidades
 
-# kilometers = 12.25
-# miles = 7.38
+kilometers = 12.25
+miles = 7.38
 
-# miles_to_kilometers = (miles * 1.61) / 1
-# kilometers_to_miles = (kilometers * 1) / 1.61
+miles_to_kilometers = (miles * 1.61) / 1
+kilometers_to_miles = (kilometers * 1) / 1.61
 
-# print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
-# print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
 
-# dollars = 3000
-# pesos_ars = 900000
+dollars = 3000
+pesos_ars = 900000
 
-# pesos_to_dollars = (pesos_ars * 1) / 1200
-# dollars_to_pesos = (dollars * 1200) / 1
+pesos_to_dollars = (pesos_ars * 1) / 1200
+dollars_to_pesos = (dollars * 1200) / 1
 
-# print(pesos_ars, "pesos son", round(pesos_to_dollars, 1), "dólares")
-# print(dollars, "dólares son", round(dollars_to_pesos, 1), "pesos")
+print(pesos_ars, "pesos son", round(pesos_to_dollars, 1), "dólares")
+print(dollars, "dólares son", round(dollars_to_pesos, 1), "pesos")
 
 # Evaluar valor de y
 
-# x =  0
-# x = float(x)
-# y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
-# print("y =", y)
+x =  0
+x = float(x)
+y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
+print("y =", y)
 
-# x =  1
-# x = float(x)
-# y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
-# print("y =", y)
+x =  1
+x = float(x)
+y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
+print("y =", y)
 
-# x =  -1
-# x = float(x)
-# y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
-# print("y =", y)
+x =  -1
+x = float(x)
+y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
+print("y =", y)
 
 
 # Operadores y expresiones
