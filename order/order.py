2c3743bae82a25b5c12178fea83d6ff9dfefc326
Leandro Magnotti <leandro.magnotti@gmail.com>
2024-06-21 13:54:39 -0300

    Trabajando con arrays

--- order/order.py
+++ order/order.py
@@ -15,3 +15,52 @@ while swapped:
 
 print("\nOrdenada:")
 print(my_list)
+
+
+# Encontrar el mayor valor en la lista
+my_list = [17, 23, 11, 25, 1, 19, 7, 15, 13]
+largest = my_list[0]
+
+for i in range(4, len(my_list)):
+    if my_list[i] > largest:
+        largest = my_list[i]
+
+print(largest)
+
+# También:
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+ 
+for i in my_list:
+    if i > largest:
+        largest = i
+ 
+print(largest)
+
+
+# Encontrar la ubicación de un elemento dado:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+to_find = 5
+found = False
+ 
+for i in range(len(my_list)):
+    found = my_list[i] == to_find
+    if found:
+        break
+ 
+if found:
+    print("Elemento encontrado en el índice", i)
+else:
+    print("ausente")
+
+
+# Encontrar una serie de números:
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+ 
+for number in bets:
+    if number in drawn:
+        hits += 1
+ 
+print(hits)
